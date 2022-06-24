76c4311d9321210c800d27f5b51420ba630eb52b
Lucas Omena <lucasomena5@gmail.com>
2022-06-24 17:37:26 -0300

    Terraform Lab 05

--- lab10_hands-on/aws-ec2/key_pair.tf
+++
@@ -1,8 +0,0 @@
-/*
-ssh-keygen -t ed25519 -C "ec2-user" -f ~/.ssh/tf-acn-workshop -N ''
-*/
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = "tf-acn-workshop"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILH8VweZauCXgguBNUTTpLXNQiez7MvC7UNqZmrvu/MP ec2-user"
-}
