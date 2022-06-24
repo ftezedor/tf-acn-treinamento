4e51eb19e718ef0a5d79de87f5bc86413cdd181d
Lucas Omena <lucasomena5@gmail.com>
2022-06-24 17:39:08 -0300

    Terraform Lab 08

--- tf-modules/sprint01/tf-module-sg/sg/output.tf
+++
@@ -1,9 +0,0 @@
-output "security_groups_id" {
-  description = "Security Groups"
-  value       = aws_security_group.sg.id
-}
-
-output "security_groups_name" {
-  description = "Security Groups"
-  value       = aws_security_group.sg.name
-}
