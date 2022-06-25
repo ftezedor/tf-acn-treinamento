a78aeac70c590eec22ca33d8082d34095c7a60fb
Lucas Omena <lucasomena5@gmail.com>
2022-06-25 08:23:20 -0300

    Terraform Lab 06

--- lab06_modules/hashicorp_module/module.tf
+++ lab06_modules/hashicorp_module/module.tf
@@ -13,7 +13,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.10.0.0/16"
 
-  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
   public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
 
