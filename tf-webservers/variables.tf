1ef48e71f1d252d430872e80d0f8d8252e675008
Lucas Omena <lucasomena5@gmail.com>
2022-07-01 11:02:03 -0300

    Terraform Challenge

--- tf-webservers/variables.tf
+++ tf-webservers/variables.tf
@@ -75,11 +75,6 @@ variable "instance_type" {
 variable "operating_system" {
   type    = string
   default = "amazon-linux"
-
-  validation {
-    condition     = contains(["amazon-linux", "centos", "ubuntu", "redhat"], var.operating_system)
-    error_message = "The current support values are amazon-linux, centos, ubuntu or redhat"
-  } 
 }
 
 variable "allowed_ports" {
