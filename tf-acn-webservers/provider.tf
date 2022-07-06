01104fd8f769304c9ec28b177459b924a34554cf
Lucas Omena <lucasomena5@gmail.com>
2022-07-06 20:42:38 -0300

    Terraform Challenge

--- tf-acn-webservers/provider.tf
+++ tf-acn-webservers/provider.tf
@@ -1,12 +1,12 @@
 ############################################# TERRAFORM #############################################
 
 terraform {
-  /* backend "s3" {
-    bucket  = "tf-state-929365926532"
-    key     = "tf-acn-treinamento/tf-acn-webservers.tfstate"
+  backend "s3" {
+    bucket  = "tf-state-083645284229"
+    key     = "tf-acn-treinamento/lucasomena/tf-acn-webservers.tfstate"
     region  = "us-east-1"
     profile = "tf-acn-treinamento"
-  } */
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
