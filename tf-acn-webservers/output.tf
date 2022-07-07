6579a2638514319e164daba3fb5b48c19dc5e1b7
Lucas Omena <lucasomena5@gmail.com>
2022-07-07 16:53:25 -0300

    Terraform Challenge

--- tf-acn-webservers/output.tf
+++ tf-acn-webservers/output.tf
@@ -1,4 +1,4 @@
 output "alb_dns_name" {
-  description = "The DNS Name of Load Balancer"  
-  value = "http://${aws_lb.alb.dns_name}/index.html"
+  description = "The DNS Name of Load Balancer"
+  value       = "http://${aws_lb.alb.dns_name}/index.html"
 }
